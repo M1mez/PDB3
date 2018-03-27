40bb2c5a2a3d6dfaea645a51f10c036001eab191
M1mez <Fpscorner@gmail.com>
2018-03-27 16:17:28 +0200

    added persinfo to jenkins

---
+++ PicDB/PersInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+
+namespace PicDB
+{
+    public static class PersInfo
+    {
+        public static string ConnString = 
+                @"Server=DESKTOP-DIN7DPC\SQLEXPRESS;
+                Database=PicDB;
+                Trusted_Connection=True;";
+
+        public static string DeployPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public static string PicPath = DeployPath + @"\Pictures";
+    }
+}
