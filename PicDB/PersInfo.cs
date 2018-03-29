2a8377da0bdc8f60c7dc8bbf9be0c43533931cfb
M1mez <Fpscorner@gmail.com>
2018-03-29 13:16:55 +0200

    new working directory getter in persinfo

--- PicDB/PersInfo.cs
+++ PicDB/PersInfo.cs
@@ -16,9 +16,9 @@ namespace PicDB
                 Trusted_Connection=True;";
 
         private static string _workingDirectory;
-        public static string DeployPath => _workingDirectory ?? (_workingDirectory = Environment.CurrentDirectory);
+        public static string DeployPath => _workingDirectory ?? (_workingDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "SWE2-CS"));
 
-        public static string PicPath = Path.Combine(DeployPath, "Pictures");
-        public static string IcoPath = Path.Combine(DeployPath, "Icons");
+        public static string PicPath => Path.Combine(DeployPath, "Pictures");
+        public static string IcoPath => Path.Combine(DeployPath, "Icons");
     }
 }
