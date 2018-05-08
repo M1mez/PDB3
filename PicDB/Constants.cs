cb2257d85bc2f42abe422295122bd8835ec92ca4
M1mez <Fpscorner@gmail.com>
2018-05-08 15:28:11 +0200

    more bool in constants to check which pc or jenkins

--- PicDB/Constants.cs
+++ PicDB/Constants.cs
@@ -12,19 +12,43 @@ namespace PicDB
     {
         private static Random rnd = new Random();
         public static int GetRandomInt() => rnd.Next();
-        private static bool JohannesPC => (Environment.MachineName == "DESKTOP-DIN7DPC");
+        private static bool JohannesPC => 
+            (Environment.MachineName == "DESKTOP-DIN7DPC");
+        private static bool StefanPC => 
+            (Environment.MachineName == "STEFFE-PC"); //TODO
+        private static bool StefanLAPTOP => 
+            (Environment.MachineName == "STEFFE"); //TODO
+
         public static string ConnString
         {
             get
             {
                 if (JohannesPC)
                     return @"Server=DESKTOP-DIN7DPC\SQLEXPRESS; Database=PicDB; Trusted_Connection=True;";
-                else return @"Server=STEFFE-PC\SQLEXPRESS; Database=PicDB; Trusted_Connection=True;";
+                if (StefanPC)
+                    return @"Server=STEFFE-PC\SQLEXPRESS; Database=PicDB; Trusted_Connection=True;"; //TODO
+                if (StefanLAPTOP)
+                    return "irgendeinPfadhalt"; //TODO
+
+                else return "";
             }
         }
 
 
-        public static string DeployPath => (JohannesPC) ? @"O:\GIT\SWE2-CS" : @"C:\Users\Steffe\Desktop\FH\4.Semester\PDB3\";
+        public static string DeployPath
+        {
+            get
+            {
+                if (JohannesPC)
+                    return @"O:\GIT\SWE2\SWE2-CS";
+                if (StefanPC)
+                    return @"C:\Users\Steffe\Desktop\FH\4.Semester\PDB3\";
+                if (StefanLAPTOP)
+                    return "PFADHALT"; //TODO
+
+                else return Environment.CurrentDirectory;
+            }
+        }
 
         /// <summary>
         /// returns Bool if the program is executed via UnitTest
